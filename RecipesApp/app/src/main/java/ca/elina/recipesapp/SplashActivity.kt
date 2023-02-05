9a5d79606998cec14f6be50d28e3555d4fa13069
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-02-05 01:43:47 -0500

    3. Splash Screen (animated logo) and app icon

---
+++ RecipesApp/app/src/main/java/ca/elina/recipesapp/SplashActivity.kt
@@ -0,0 +1,66 @@
+package ca.elina.recipesapp
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import ca.elina.recipesapp.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
+        // Splash Activity as a full screen view - hidden Status Bar.
+        // START
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        // END
+
+        // Access variable for Animation
+        // START
+        val splashAnimation = AnimationUtils.loadAnimation(this@SplashActivity, R.anim.anim_splash)
+        splashBinding.ivLogo.animation = splashAnimation
+        // END
+
+        // Perform any action after animation completion with the callbacks as below.
+        // START
+        splashAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                // "Add the code that you want to execute when animation starts")
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                // "Add the code that you want to execute when animation ends")
+
+                // Once the animation completes, navigate to the Main Activity with delay 1 second (1000 milliseconds) using Handler.
+                // START
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }, 2000)
+                // END
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                // "Add the code that you want to execute when animation repeats")
+            }
+        })
+        // END
+    }
+}
