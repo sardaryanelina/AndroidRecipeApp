35d15d7185caf1c95774a5e5e140d66b0c94d72a
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-02-06 08:38:06 -0500

    Replace back button

--- RecipesApp/app/src/main/java/ca/elina/recipesapp/view/activities/AddUpdateRecipeActivity.kt
+++ RecipesApp/app/src/main/java/ca/elina/recipesapp/view/activities/AddUpdateRecipeActivity.kt
@@ -2,10 +2,11 @@ package ca.elina.recipesapp.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import ca.elina.recipesapp.R
 import ca.elina.recipesapp.databinding.ActivityAddUpdateRecipeBinding
 
-class AddUpdateRecipeActivity : AppCompatActivity() {
+class AddUpdateRecipeActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mBinding: ActivityAddUpdateRecipeBinding
 
@@ -22,11 +23,21 @@ class AddUpdateRecipeActivity : AppCompatActivity() {
      * A function for ActionBar setup.
      */
     private fun setupActionBar() {
+        // action bar in xml -> id is toolbar_add_recipe_activity
+        // we are assigning this bar using default method setSupportActionBar
         setSupportActionBar(mBinding.toolbarAddDishActivity)
 
+        // this will allow us to have back button
         supportActionBar?.setDisplayHomeAsUpEnabled(true) // back button in toolbar
 
-        // click listener to back button in toolbar
+        // Replace the back button that we have generated.
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+
+        // add click listener to back button
         mBinding.toolbarAddDishActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
