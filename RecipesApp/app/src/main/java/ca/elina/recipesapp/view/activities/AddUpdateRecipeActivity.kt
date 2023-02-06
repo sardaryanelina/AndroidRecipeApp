aba656080e210dd73606bc730c46f434e208b36f
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-02-06 14:10:39 -0500

    6. Setup Permissions - Camera and Storage Permissions

--- RecipesApp/app/src/main/java/ca/elina/recipesapp/view/activities/AddUpdateRecipeActivity.kt
+++ RecipesApp/app/src/main/java/ca/elina/recipesapp/view/activities/AddUpdateRecipeActivity.kt
@@ -1,13 +1,24 @@
 package ca.elina.recipesapp.view.activities
 
+import android.Manifest
 import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import ca.elina.recipesapp.R
 import ca.elina.recipesapp.databinding.ActivityAddUpdateRecipeBinding
 import ca.elina.recipesapp.databinding.DialogCustomImageSelectionBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class AddUpdateRecipeActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -64,18 +75,106 @@ class AddUpdateRecipeActivity : AppCompatActivity(), View.OnClickListener {
         The resource will be inflated, adding all top-level views to the screen.*/
         dialog.setContentView(binding.root)
 
-        // Assign the click for Camera and Gallery. Show the Toast message for now.
+        // Assign the click for Camera.
         binding.tvCamera.setOnClickListener {
-            Toast.makeText(this, "You have clicked on the Camera.", Toast.LENGTH_SHORT).show()
+
+            // Ask for the permission while selecting the image from camera using Dexter Library.
+            // Comment out or Remove the toast message.
+            // Toast.makeText(this, "You have clicked on the Camera.", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this)
+                .withPermissions(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA
+                )
+                .withListener(object : MultiplePermissionsListener {
+
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        // Here after all the permission are granted launch the CAMERA to capture an image.
+                        if (report!!.areAllPermissionsGranted()) {
+
+                            // Show the Toast message for now just to know that we have the permission.
+                            Toast.makeText(this@AddUpdateRecipeActivity,"You have the Camera permission now to capture image.",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        // Show the alert dialog
+                        showRationalDialogForPermissions()
+
+                    }
+                }).onSameThread()
+                .check()
+
+            // dismiss the dialog in the end
             dialog.dismiss()
         }
 
+        // Assign the click for Gallery.
         binding.tvGallery.setOnClickListener {
-            Toast.makeText(this, "You have clicked on the Gallery.", Toast.LENGTH_SHORT).show()
+
+            // Ask for the permission while selecting the image from Gallery using Dexter Library.
+            // Comment out or Remove the toast message.
+            // Toast.makeText(this, "You have clicked on the Gallery.", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this@AddUpdateRecipeActivity)
+                .withPermissions(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+                .withListener(object : MultiplePermissionsListener {
+
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+
+                        // Here after all the permission are granted launch the gallery to select and image.
+                        if (report!!.areAllPermissionsGranted()) {
+                            // Show the Toast message for now just to know that we have the permission.
+                            Toast.makeText(this@AddUpdateRecipeActivity,"You have the Gallery permission now to select image.",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
+                    // If you want to receive permission listener callbacks on the same thread that fired the permission request,
+                    // you just need to call onSameThread before checking for permissions:
+                }).onSameThread()
+                .check()
+
+            // dismiss the dialog in the end
             dialog.dismiss()
         }
 
         //Start the dialog and display it on screen.
         dialog.show()
+    }
+
+    /**
+     * A function used to show the alert dialog when the permissions are denied and need to allow it from settings app info.
+     */
+    //Create a function to show the alert message that the permission is necessary to proceed further if user deny it. And ask him to allow it from setting.
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton(
+                "GO TO SETTINGS"
+            ) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 }
