76e4e798baae3a8bbe652f88e5e982085a0a6231
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-02-06 08:28:49 -0500

    Setting up back button in toolbar

--- RecipesApp/app/src/main/java/ca/elina/recipesapp/view/activities/AddUpdateRecipeActivity.kt
+++ RecipesApp/app/src/main/java/ca/elina/recipesapp/view/activities/AddUpdateRecipeActivity.kt
@@ -3,10 +3,30 @@ package ca.elina.recipesapp.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ca.elina.recipesapp.R
+import ca.elina.recipesapp.databinding.ActivityAddUpdateRecipeBinding
 
 class AddUpdateRecipeActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityAddUpdateRecipeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_recipe)
+
+        mBinding = ActivityAddUpdateRecipeBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupActionBar()
+    }
+
+    /**
+     * A function for ActionBar setup.
+     */
+    private fun setupActionBar() {
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // back button in toolbar
+
+        // click listener to back button in toolbar
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener { onBackPressed() }
     }
 }
