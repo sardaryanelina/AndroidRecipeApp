34c597d82607c7f82bc473ec6d20640437310c53
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-02-19 16:56:47 -0500

    Clean Code

--- RecipesApp/app/src/main/java/ca/elina/recipesapp/view/fragments/RecipesFragment.kt
+++ RecipesApp/app/src/main/java/ca/elina/recipesapp/view/fragments/RecipesFragment.kt
@@ -7,18 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import ca.elina.recipesapp.R
 import ca.elina.recipesapp.adapters.RecipesAdapter
 import ca.elina.recipesapp.databinding.FragmentRecipesBinding
 import ca.elina.recipesapp.util.NetworkResult
 import ca.elina.recipesapp.viewmodel.MainViewModel
 import ca.elina.recipesapp.viewmodel.RecipesViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import com.facebook.shimmer.ShimmerFrameLayout;
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class RecipesFragment : Fragment() {
