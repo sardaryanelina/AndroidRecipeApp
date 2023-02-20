51e5e505cad4adcfd660dec7509f5b809763693e
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-02-20 11:50:09 -0500

    39. Modify and Inject RecipesViewModel

--- RecipesApp/app/src/main/java/ca/elina/recipesapp/viewmodel/RecipesViewModel.kt
+++ RecipesApp/app/src/main/java/ca/elina/recipesapp/viewmodel/RecipesViewModel.kt
@@ -2,6 +2,8 @@ package ca.elina.recipesapp.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import ca.elina.recipesapp.data.DataStoreRepository
 import ca.elina.recipesapp.util.Constants.Companion.API_KEY
 import ca.elina.recipesapp.util.Constants.Companion.DEFAULT_DIET_TYPE
 import ca.elina.recipesapp.util.Constants.Companion.DEFAULT_MEAL_TYPE
@@ -12,16 +14,39 @@ import ca.elina.recipesapp.util.Constants.Companion.QUERY_DIET
 import ca.elina.recipesapp.util.Constants.Companion.QUERY_FILL_INGREDIENTS
 import ca.elina.recipesapp.util.Constants.Companion.QUERY_NUMBER
 import ca.elina.recipesapp.util.Constants.Companion.QUERY_TYPE
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class RecipesViewModel(application: Application) : AndroidViewModel(application) {
+class RecipesViewModel @Inject constructor(
+    application: Application,
+    private val dataStoreRepository: DataStoreRepository
+) : AndroidViewModel(application) {
+
+    private var mealType = DEFAULT_MEAL_TYPE
+    private var dietType = DEFAULT_DIET_TYPE
+
+    val readMealAndDietType = dataStoreRepository.readMealAndDietType
+
+    fun saveMealAndDietType(mealType: String, mealTypeId: Int, dietType: String, dietTypeId: Int) =
+        viewModelScope.launch(Dispatchers.IO) {
+            dataStoreRepository.saveMealAndDietType(mealType, mealTypeId, dietType, dietTypeId)
+        }
 
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
 
+        viewModelScope.launch {
+            readMealAndDietType.collect { value ->
+                mealType = value.selectedMealType
+                dietType = value.selectedDietType
+            }
+        }
+
         queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[QUERY_API_KEY] = API_KEY
-        queries[QUERY_TYPE] = DEFAULT_MEAL_TYPE
-        queries[QUERY_DIET] = DEFAULT_DIET_TYPE
+        queries[QUERY_TYPE] = mealType
+        queries[QUERY_DIET] = dietType
         queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[QUERY_FILL_INGREDIENTS] = "true"
 
