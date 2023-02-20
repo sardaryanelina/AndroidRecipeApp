593a95989636287b2af9cafb317d259e58d5b660
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-02-19 21:38:20 -0500

    28. Create DatabaseModule

---
+++ RecipesApp/app/src/main/java/ca/elina/recipesapp/di/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package ca.elina.recipesapp.di
+
+import android.content.Context
+import androidx.room.Room
+import ca.elina.recipesapp.data.database.RecipesDatabase
+import ca.elina.recipesapp.util.Constants.Companion.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides // for third party class
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        RecipesDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: RecipesDatabase) = database.recipesDao()
+
+}
