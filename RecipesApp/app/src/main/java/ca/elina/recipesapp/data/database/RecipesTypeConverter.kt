dc25b70e1f5ebb34ae2913025ad4b5e486162011
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-02-19 21:25:04 -0500

    26. Create Type Converter (RecipesTypeConverter) by using Gson library

---
+++ RecipesApp/app/src/main/java/ca/elina/recipesapp/data/database/RecipesTypeConverter.kt
@@ -0,0 +1,22 @@
+package ca.elina.recipesapp.data.database
+
+import androidx.room.TypeConverter
+import ca.elina.recipesapp.models.FoodRecipe
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class RecipesTypeConverter {
+
+    var gson = Gson()
+
+    @TypeConverter
+    fun foodRecipeToString(foodRecipe: FoodRecipe): String {
+        return gson.toJson(foodRecipe) // we are returning String
+    }
+
+    @TypeConverter
+    fun stringToFoodRecipe(data: String): FoodRecipe {
+        val listType = object : TypeToken<FoodRecipe>() {}.type
+        return gson.fromJson(data, listType)
+    }
+}
