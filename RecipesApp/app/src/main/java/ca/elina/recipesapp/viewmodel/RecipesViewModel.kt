f84f88d649522b5b6fbacc6c0e74e8ab0c65acef
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-02-20 16:21:59 -0500

    43. RecipesFragment - Listen for Internet Connection

--- RecipesApp/app/src/main/java/ca/elina/recipesapp/viewmodel/RecipesViewModel.kt
+++ RecipesApp/app/src/main/java/ca/elina/recipesapp/viewmodel/RecipesViewModel.kt
@@ -1,6 +1,7 @@
 package ca.elina.recipesapp.viewmodel
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import ca.elina.recipesapp.data.DataStoreRepository
@@ -26,6 +27,8 @@ class RecipesViewModel @Inject constructor(
 
     private var mealType = DEFAULT_MEAL_TYPE
     private var dietType = DEFAULT_DIET_TYPE
+
+    var networkStatus = false
 
     val readMealAndDietType = dataStoreRepository.readMealAndDietType
 
@@ -54,4 +57,9 @@ class RecipesViewModel @Inject constructor(
         return queries
     }
 
+    fun showNetworkStatus() {
+        if (!networkStatus) {
+            Toast.makeText(getApplication(), "No Internet Connection.", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
