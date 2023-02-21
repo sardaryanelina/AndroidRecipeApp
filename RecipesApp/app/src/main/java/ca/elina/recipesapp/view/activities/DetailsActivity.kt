2d28a7d660d763a86b8b1a8fa5f77e6fbe9ac80e
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-02-20 22:19:13 -0500

    50. Design activity_details Layout

---
+++ RecipesApp/app/src/main/java/ca/elina/recipesapp/view/activities/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package ca.elina.recipesapp.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ca.elina.recipesapp.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
