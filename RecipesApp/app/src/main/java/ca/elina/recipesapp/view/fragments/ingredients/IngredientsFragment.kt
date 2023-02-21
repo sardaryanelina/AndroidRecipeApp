320e8d68aaa2e2c5d0e23ed50477f20dad0d7113
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-02-21 17:54:37 -0500

    59. IngredientsFragment - Display Ingredients Data

--- RecipesApp/app/src/main/java/ca/elina/recipesapp/view/fragments/ingredients/IngredientsFragment.kt
+++ RecipesApp/app/src/main/java/ca/elina/recipesapp/view/fragments/ingredients/IngredientsFragment.kt
@@ -5,16 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ca.elina.recipesapp.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import ca.elina.recipesapp.models.Result
+import ca.elina.recipesapp.adapters.IngredientsAdapter
+import ca.elina.recipesapp.databinding.FragmentIngredientsBinding
+import ca.elina.recipesapp.util.Constants.Companion.RECIPE_RESULT_KEY
 
 class IngredientsFragment : Fragment() {
+
+    private val mAdapter: IngredientsAdapter by lazy { IngredientsAdapter() }
+
+    private var _binding: FragmentIngredientsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ingredients, container, false)
+        _binding = FragmentIngredientsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(RECIPE_RESULT_KEY)
+
+        setupRecyclerView()
+        myBundle?.extendedIngredients?.let { mAdapter.setData(it) }
+
+        return binding.root
+    }
+
+    private fun setupRecyclerView() {
+        binding.ingredientsRecyclerview.adapter = mAdapter
+        binding.ingredientsRecyclerview.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
