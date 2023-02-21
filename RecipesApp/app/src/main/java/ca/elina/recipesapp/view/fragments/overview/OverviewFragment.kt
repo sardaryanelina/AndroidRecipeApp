bb37caa1a38bc6d9d85da49b869aadf1fdddba67
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-02-21 00:23:52 -0500

    54. OverviewFragment - Display Recipe Data

--- RecipesApp/app/src/main/java/ca/elina/recipesapp/view/fragments/overview/OverviewFragment.kt
+++ RecipesApp/app/src/main/java/ca/elina/recipesapp/view/fragments/overview/OverviewFragment.kt
@@ -5,16 +5,65 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import ca.elina.recipesapp.R
+import ca.elina.recipesapp.bindingadapters.RecipesRowBinding
+import ca.elina.recipesapp.databinding.FragmentOverviewBinding
+import ca.elina.recipesapp.models.Result
+import coil.load
 
 class OverviewFragment : Fragment() {
+
+    private var _binding: FragmentOverviewBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false)
+        _binding = FragmentOverviewBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable("recipeBundle")
+
+        binding.mainImageView.load(myBundle?.image)
+        binding.titleTextView.text = myBundle?.title
+        binding.likesTextView.text = myBundle?.aggregateLikes.toString()
+        binding.timeTextView.text = myBundle?.readyInMinutes.toString()
+        binding.summaryTextView.text = myBundle?.summary
+
+        if(myBundle?.vegetarian == true){
+            binding.vegetarianImageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            binding.vegetarianTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        if(myBundle?.vegan == true){
+            binding.veganImageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            binding.veganTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        if(myBundle?.glutenFree == true){
+            binding.glutenFreeImageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            binding.glutenFreeTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        if(myBundle?.dairyFree == true){
+            binding.dairyFreeImageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            binding.dairyFreeTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        if(myBundle?.veryHealthy == true){
+            binding.healthyImageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            binding.healthyTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        if(myBundle?.cheap == true){
+            binding.cheapImageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            binding.cheapTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        return binding.root
     }
 
 }
