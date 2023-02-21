77f48a0f2692c13ab7c2780b7d2b34d6ba328f5b
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-02-21 18:41:01 -0500

    64. Modify RecipesDao interface

--- RecipesApp/app/src/main/java/ca/elina/recipesapp/data/database/RecipesDao.kt
+++ RecipesApp/app/src/main/java/ca/elina/recipesapp/data/database/RecipesDao.kt
@@ -1,6 +1,7 @@
 package ca.elina.recipesapp.data.database
 
 import androidx.room.*
+import ca.elina.recipesapp.data.database.entities.FavoritesEntity
 import ca.elina.recipesapp.data.database.entities.RecipesEntity
 
 import kotlinx.coroutines.flow.Flow
@@ -13,4 +14,16 @@ interface RecipesDao {
 
     @Query("SELECT * FROM recipes_table ORDER BY id ASC")
     fun readRecipes(): Flow<List<RecipesEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavoriteRecipe(favoritesEntity: FavoritesEntity)
+
+    @Query("SELECT * FROM favorite_recipes_table ORDER BY id ASC")
+    fun readFavoriteRecipes(): Flow<List<FavoritesEntity>>
+
+    @Delete
+    suspend fun deleteFavoriteRecipe(favoritesEntity: FavoritesEntity)
+
+    @Query("DELETE FROM favorite_recipes_table")
+    suspend fun deleteAllFavoriteRecipes()
 }
