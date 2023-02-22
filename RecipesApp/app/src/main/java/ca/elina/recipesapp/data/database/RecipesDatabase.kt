b515a0d698ae03882bf2390ccd7eb922d0aba0d0
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-02-21 19:07:12 -0500

    67. Modify RecipesDatabase class

--- RecipesApp/app/src/main/java/ca/elina/recipesapp/data/database/RecipesDatabase.kt
+++ RecipesApp/app/src/main/java/ca/elina/recipesapp/data/database/RecipesDatabase.kt
@@ -3,10 +3,11 @@ package ca.elina.recipesapp.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import ca.elina.recipesapp.data.database.entities.FavoritesEntity
 import ca.elina.recipesapp.data.database.entities.RecipesEntity
 
 @Database(
-    entities = [RecipesEntity::class],
+    entities = [RecipesEntity::class, FavoritesEntity::class],
     version = 1, // whenever we change the database schema, we need to increase the version number
     exportSchema = false // tell the room do not export the database schema into a folder,
     // it is optional and good for version control system, but we do not use it here
