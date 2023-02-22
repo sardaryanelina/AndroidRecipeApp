f970275d0a9c2cbbd8ef2a9d043b78c7184b63f4
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-02-22 09:48:28 -0500

    79. Search in Map preparation - not finished functionality

--- RecipesApp/app/src/main/java/ca/elina/recipesapp/view/fragments/map/NearMeFragment.kt
+++ RecipesApp/app/src/main/java/ca/elina/recipesapp/view/fragments/map/NearMeFragment.kt
@@ -5,20 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ca.elina.recipesapp.R
-
-/**
- * A simple [Fragment] subclass.
- * Use the [NearMeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+import ca.elina.recipesapp.databinding.FragmentNearMeBinding
 class NearMeFragment : Fragment() {
+
+    private var _binding: FragmentNearMeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_near_me, container, false)
+        _binding = FragmentNearMeBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 }
