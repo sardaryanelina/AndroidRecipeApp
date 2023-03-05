930251f5c88e9a923683aa6d1a95528c1c4de651
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-03-04 19:06:08 -0500

    81. Search in Google Map

--- RecipesApp/app/src/main/java/ca/elina/recipesapp/view/fragments/map/NearMeFragment.kt
+++ RecipesApp/app/src/main/java/ca/elina/recipesapp/view/fragments/map/NearMeFragment.kt
@@ -6,8 +6,12 @@ import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.location.LocationManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Looper
 import android.provider.Settings
@@ -15,14 +19,19 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import ca.elina.recipesapp.R
 import ca.elina.recipesapp.databinding.FragmentNearMeBinding
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
@@ -35,14 +44,22 @@ import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.io.IOException
 
-class NearMeFragment : Fragment() {
+class NearMeFragment : Fragment(), OnMapReadyCallback, LocationListener,
+    GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private var _binding: FragmentNearMeBinding? = null
     private val binding get() = _binding!!
 
     private var currentPosition: LatLng? = null
 
+    // Search Google Map
+    private var mMap: GoogleMap? = null
+    internal lateinit var mLastLocation: Location
+    internal var mCurrLocationMarker: Marker? = null
+    internal var mGoogleApiClient: GoogleApiClient? = null
+    internal lateinit var mLocationRequest: LocationRequest
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -57,6 +74,37 @@ class NearMeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         getUserCurrentLocation()
+
+        binding.imageViewSearch.setOnClickListener {
+            val locationSearch: EditText = binding.inputLocation
+            var location: String = locationSearch.text.toString().trim()
+            var addressList: List<Address>? = null
+
+            if (location == null || location == "") {
+                Toast.makeText(this.requireContext(), "provide location", Toast.LENGTH_SHORT).show()
+            } else {
+                val geoCoder = Geocoder(this.requireContext())
+                try {
+                    addressList = geoCoder.getFromLocationName(location, 1)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+                val address = addressList!![0]
+                if (address != null) {
+
+                    val latLng = LatLng(address.latitude, address.longitude)
+                    if (mMap != null) {
+                        mMap!!.addMarker(MarkerOptions().position(latLng).title(location))
+                        mMap!!.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+                    }
+                }
+            }
+        }
+
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
     }
 
     private fun getUserCurrentLocation() {
@@ -112,11 +160,15 @@ class NearMeFragment : Fragment() {
                 currentPosition = LatLng(currentLat, currentLong)
                 callback =
                     OnMapReadyCallback { googleMap ->
-                        googleMap.addMarker(MarkerOptions().position(currentPosition!!).title("You are here"))
+                        googleMap.addMarker(
+                            MarkerOptions().position(currentPosition!!).title("You are here")
+                        )
                         googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentPosition!!))
 
-                        val newLatLongZoom = CameraUpdateFactory.newLatLngZoom(currentPosition!!, 14f)
-                        googleMap.animateCamera(newLatLongZoom)}
+                        val newLatLongZoom =
+                            CameraUpdateFactory.newLatLngZoom(currentPosition!!, 14f)
+                        googleMap.animateCamera(newLatLongZoom)
+                    }
 
                 val mapsFragment =
                     childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment?
@@ -229,4 +281,73 @@ class NearMeFragment : Fragment() {
                 }
             })
         }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    this.requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                buildGoogleApiClient()
+                mMap!!.isMyLocationEnabled = true
+            }
+        } else {
+            buildGoogleApiClient()
+            mMap!!.isMyLocationEnabled = true
+        }
+    }
+
+    protected fun buildGoogleApiClient() {
+        mGoogleApiClient = GoogleApiClient.Builder(this.requireContext())
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API).build()
+        mGoogleApiClient!!.connect()
+    }
+
+    override fun onLocationChanged(location: Location) {
+        mLastLocation = location
+        if (mCurrLocationMarker != null) {
+            mCurrLocationMarker!!.remove()
+        }
+
+        val latLng = LatLng(location.latitude, location.longitude)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(latLng)
+        markerOptions.title("Current Position")
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+        mCurrLocationMarker = mMap!!.addMarker(markerOptions)
+
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+        mMap!!.moveCamera(CameraUpdateFactory.zoomTo(11f))
+
+        if (mGoogleApiClient != null) {
+            LocationServices.getFusedLocationProviderClient(this.requireContext())
+        }
+    }
+
+    override fun onConnected(p0: Bundle?) {
+
+        mLocationRequest = LocationRequest()
+        mLocationRequest.interval = 1000
+        mLocationRequest.fastestInterval = 1000
+        mLocationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        if (ContextCompat.checkSelfPermission(
+                this.requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            LocationServices.getFusedLocationProviderClient(this.requireContext())
+        }
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+
+    }
 }
